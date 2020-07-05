21a962b81245e54ada8fc53573a476b4abc4cd5a
Akshay <er.akshaypathak@gmail.com>
2020-07-05 16:51:47 +0530

    change driver

--- src/test/java/com/shoppingCart/PageObjectModel/PageObjectModel.java
+++ src/test/java/com/shoppingCart/PageObjectModel/PageObjectModel.java
@@ -1,5 +1,7 @@
 package com.shoppingCart.PageObjectModel;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
@@ -8,6 +10,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.shoppingCart.appModule.AddProduct;
 import com.shoppingCart.appModule.CheckOutProcess;
@@ -16,6 +20,7 @@ import com.shoppingCart.appModule.SignInAction;
 import com.shoppingCart.pageObjectClass.HistoryPage;
 import com.shoppingCart.pageObjectClass.HomePage;
 import com.shoppingCart.utility.Constant;
+import com.worldline.msp.integration.test.msp.RobustRemoteWebDriver;
 
 
 
@@ -25,11 +30,25 @@ public class PageObjectModel {
 	private static final Logger log = Logger.getLogger(PageObjectModel.class);
 	private static WebDriver driver = null;
 	  
+//	protected static RemoteWebDriver driver;
 	  @BeforeClass
-	  public static void launchBrowser() {
+	  public static void launchBrowser() throws MalformedURLException {
 		  
-		  System.setProperty("webdriver.gecko.driver","Path for yor geckodriver exe files");
-			driver=new FirefoxDriver();
+//		  System.setProperty("webdriver.gecko.driver","G:\\Study\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+	
+			DesiredCapabilities caps = new DesiredCapabilities();
+
+			caps.setCapability("name", "Selenium Test Example");
+			caps.setCapability("build", "1.0");
+			caps.setCapability("browser_api_name", "Edge20");
+			caps.setCapability("os_api_name", "Win10");
+			caps.setCapability("screen_resolution", "1024x768");
+			caps.setCapability("selenium_version", "2.53.1");
+
+//			 driver = new RemoteWebDriver(
+//					new URL("http://"), caps);
+		  
+		  driver=new FirefoxDriver();
 			
 			log.info("Browser is launched");
 			
